3f5aba28dd741ae1f1b5db6b755e11422403581a
Romário Ferreira <romarioferreirarezende@hotmail.com>
2022-07-15 11:27:43 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
 import NotificationButton from "./components/NotificationButton";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
   return (
     <>
+    <ToastContainer/>
       <Header />
       <main>
         <section id="sales">
